91cc99f73af67baea2c24811a9fb52c71a6c483b
Tobias Kirschstein <lev@ideegeo.com>
2013-05-03 05:35:56 +1200

    migrated to dist::zilla

--- lib/WebService/Desk.pm
+++ lib/WebService/Desk.pm
@@ -1,20 +1,41 @@
 package WebService::Desk;
 
 use 5.010;
-use Any::Moose;
+use Mouse;
+
+# ABSTRACT: WebService::Desk - an interface to desk.com's RESTful Web API using Web::API
+
+# VERSION
+
 with 'Web::API';
 
-=head1 NAME
+=head1 SYNOPSIS
 
-WebService::Desk - an interface to desk.com's RESTful Web API using Web::API
+Please refer to the API documentation at L<http://dev.desk.com/docs/api>
 
-=head1 VERSION
+    use WebService::Desk;
+    
+    my $desk = WebService::Desk->new(
+        debug   => 1,
+        api_key => '12345678-9abc-def0-1234-56789abcdef0',
+    );
+    
+    my $response = $desk->create_interaction(
+        subject      => "h4x0r",
+        from_email   => "mail@example.com",
+        text         => "what zee fug",
+        track_opens  => 1,
+        track_clicks => 1,
+        to => [
+            { email => 'mail@example.com' }
+        ],
+    );
 
-Version 0.1
+=head1 SUBROUTINES/METHODS
+
+=head2 ping
 
 =cut
-
-our $VERSION = '0.1';
 
 has 'commands' => (
     is      => 'rw',
@@ -147,32 +168,6 @@ has 'commands' => (
     },
 );
 
-=head1 SYNOPSIS
-
-Please refer to the API documentation at L<http://dev.desk.com/docs/api>
-
-    use WebService::Desk;
-    
-    my $desk = WebService::Desk->new(
-        debug   => 1,
-        api_key => '12345678-9abc-def0-1234-56789abcdef0',
-    );
-    
-    my $response = $desk->create_interaction(
-        subject      => "h4x0r",
-        from_email   => "mail@example.com",
-        text         => "what zee fug",
-        track_opens  => 1,
-        track_clicks => 1,
-        to => [
-            { email => 'mail@example.com' }
-        ],
-    );
-
-=head1 SUBROUTINES/METHODS
-
-=head2 ping
-
 =head1 INTERNALS
 
 =cut
@@ -200,15 +195,10 @@ sub BUILD {
     return $self;
 }
 
-=head1 AUTHOR
-
-Tobias Kirschstein, C<< <lev at cpan.org> >>
-
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-WebService-Desk at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WebService-Desk>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests on GitHub's issue tracker L<https://github.com/nupfel/WebService-Desk/issues>.
+
 
 =head1 SUPPORT
 
@@ -216,38 +206,28 @@ You can find documentation for this module with the perldoc command.
 
     perldoc WebService::Desk
 
+
 You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker (report bugs here)
+=item * GitHub repository
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WebService-Desk>
+L<https://github.com/nupfel/WebService-Desk>
+
+=item * MetaCPAN
+
+L<https://metacpan.org/module/WebService::Desk>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
-L<http://annocpan.org/dist/WebService-Desk>
+L<http://annocpan.org/dist/WebService::Desk>
 
 =item * CPAN Ratings
 
-L<http://cpanratings.perl.org/d/WebService-Desk>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/WebService-Desk/>
+L<http://cpanratings.perl.org/d/WebService::Desk>
 
 =back
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 Tobias Kirschstein.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
 
 =cut
 
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-    use_ok( 'WebService::Desk' ) || print "Bail out!\n";
-}
-
-diag( "Testing WebService::Desk $WebService::Desk::VERSION, Perl $], $^X" );
