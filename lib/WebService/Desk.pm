f7e2c5c1287c850c1db6ab2a99a4437c606fe3c0
Tobias Kirschstein <lev@ideegeo.com>
2013-05-06 17:24:44 +1200

    make use of multiple IDs in path

--- lib/WebService/Desk.pm
+++ lib/WebService/Desk.pm
@@ -42,47 +42,34 @@ has 'commands' => (
     default => sub {
         {
             # cases
-            cases => { path        => 'cases' },
-            case  => { pre_id_path => 'cases', require_id => 1 },
-            update_case =>
-                { method => 'PUT', pre_id_path => 'cases', require_id => 1 },
+            cases       => { path   => 'cases' },
+            case        => { path   => 'cases/:id' },
+            update_case => { method => 'PUT', path => 'cases/:id' },
 
             # customers
-            customers       => { path        => 'customers' },
-            customer        => { pre_id_path => 'customers', require_id => 1 },
-            create_customer => { method      => 'POST', path => 'customers' },
-            update_customer => {
-                method      => 'PUT',
-                pre_id_path => 'customers',
-                require_id  => 1
-            },
+            customers       => { path   => 'customers' },
+            customer        => { path   => 'customers/:id' },
+            create_customer => { method => 'POST', path => 'customers' },
+            update_customer => { method => 'PUT', path => 'customers/:id' },
             create_customer_email => {
-                method       => 'POST',
-                pre_id_path  => 'customers',
-                post_id_path => 'emails',
-                require_id   => 1,
-                mandatory    => ['email'],
+                method    => 'POST',
+                path      => 'customers/:id/emails',
+                mandatory => ['email'],
             },
             update_customer_email => {
-                method       => 'PUT',
-                pre_id_path  => 'customers',
-                post_id_path => 'emails/:id',
-                require_id   => 1,
-                mandatory    => ['email'],
+                method    => 'PUT',
+                path      => 'customers/:id/emails/:email_id',
+                mandatory => ['email'],
             },
             create_customer_phone => {
-                method       => 'POST',
-                pre_id_path  => 'customers',
-                post_id_path => 'phones',
-                require_id   => 1,
-                mandatory    => ['phone'],
+                method    => 'POST',
+                path      => 'customers/:id/phones',
+                mandatory => ['phone'],
             },
             update_customer_phone => {
-                method       => 'PUT',
-                pre_id_path  => 'customers',
-                post_id_path => 'phones/:id',
-                require_id   => 1,
-                mandatory    => ['phone'],
+                method    => 'PUT',
+                path      => 'customers/:id/phones/:phone_id',
+                mandatory => ['phone'],
             },
 
             interactions       => { path => 'interactions' },
@@ -93,76 +80,54 @@ has 'commands' => (
             },
 
             # users/groups
-            groups => { path        => 'groups' },
-            group  => { pre_id_path => 'groups', require_id => 1 },
-            users  => { path        => 'users' },
-            user   => { pre_id_path => 'users', require_id => 1 },
+            groups    => { path => 'groups' },
+            get_group => { path => 'groups/:id' },
+            users     => { path => 'users' },
+            get_user  => { path => 'users/:id' },
 
             # topics
-            topics => { path        => 'topics' },
-            topics => { pre_id_path => 'topics', require_id => 1 },
+            topics => { path => 'topics' },
+            topic  => { path => 'topics/:id' },
             create_topic =>
                 { method => 'POST', path => 'topics', mandatory => ['name'], },
-            update_topic =>
-                { method => 'PUT', pre_id_path => 'topics', require_id => 1, },
+            update_topic => { method => 'PUT', path => 'topics/:id' },
             delete_topic => {
-                method      => 'DELETE',
-                pre_id_path => 'topics',
-                require_id  => 1,
+                method => 'DELETE',
+                path   => 'topics/:id',
             },
-            topic_articles => {
-                pre_id_path  => 'topics',
-                post_id_path => 'articles',
-                require_id   => 1,
-            },
+            topic_articles => { path => 'topics/:id/articles', },
             create_article => {
-                method       => 'POST',
-                pre_id_path  => 'topics',
-                post_id_path => 'articles',
-                require_id   => 1,
-                mandatory    => ['subject'],
+                method    => 'POST',
+                path      => 'topics/:id/articles',
+                mandatory => ['subject'],
             },
-            article        => { pre_id_path => 'articles', require_id => 1, },
+            article        => { path => 'articles/:id' },
             update_article => {
-                method      => 'PUT',
-                pre_id_path => 'articles',
-                require_id  => 1,
+                method => 'PUT',
+                path   => 'articles/:id',
             },
             delete_article => {
-                method      => 'DELETE',
-                pre_id_path => 'articles',
-                require_id  => 1,
+                method => 'DELETE',
+                path   => 'articles/:id',
             },
 
             # macros
-            macros => { path        => 'macros' },
-            macro  => { pre_id_path => 'macros', require_id => 1 },
+            macros => { path => 'macros' },
+            macro  => { path => 'macros/:id' },
             create_macro =>
                 { method => 'POST', path => 'macros', mandatory => ['name'], },
-            update_macro =>
-                { method => 'PUT', pre_id_path => 'macros', require_id => 1, },
+            update_macro => { method => 'PUT', path => 'macros/:id' },
             delete_macro => {
-                method      => 'DELETE',
-                pre_id_path => 'macros',
-                require_id  => 1,
+                method => 'DELETE',
+                path   => 'macros/:id',
             },
 
             # macro actions
-            macro_actions => {
-                pre_id_path  => 'macros',
-                post_id_path => 'actions',
-                require_id   => 1,
-            },
-            macro_action => {
-                pre_id_path  => 'macros',
-                post_id_path => 'actions/:slug',
-                require_id   => 1
-            },
+            macro_actions       => { path => 'macros/:id/actions', },
+            macro_action        => { path => 'macros/:id/actions/:slug', },
             update_macro_action => {
-                method       => 'PUT',
-                pre_id_path  => 'macros',
-                post_id_path => 'actions/:action_type',
-                require_id   => 1,
+                method => 'PUT',
+                path   => 'macros/:id/actions/:action_type',
             },
         };
     },
@@ -186,7 +151,7 @@ basic configuration for the client API happens usually in the BUILD method when 
 sub BUILD {
     my ($self) = @_;
 
-    $self->user_agent(__PACKAGE__ . ' ' . $VERSION);
+    $self->user_agent(__PACKAGE__ . ' ' . $WebService::Desk::VERSION);
     $self->content_type('application/json');
     $self->extension('json');
     $self->base_url('https://' . $self->user . '.desk.com/api/v1');
